e02984cec6294ee7fd8f97506ec49e59708ddb8f
Bambo <student-wroclaw@EDU-WSB.local>
2024-12-07 14:16:13 +0100

    drugie cwiczenie

---
+++ cwiczenie2.py
@@ -0,0 +1,13 @@
+#lista owocow
+
+owoce = ["banan", "kaki", "morele"]
+
+#słownik z cenami
+
+ceny = {"banan": 2,"kaki": 10,"morele": 3}
+
+#wyswietlenie
+#print(ceny)
+
+ulubiony = owoce[1]
+print(f"Mój ulubiony owoc to {ulubiony} i kosztuje {ceny[ulubiony]}")
