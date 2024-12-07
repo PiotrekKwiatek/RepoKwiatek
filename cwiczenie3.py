551bb62f700402cc3980f29fb0b15105ea8a078c
Bambo <student-wroclaw@EDU-WSB.local>
2024-12-07 14:28:57 +0100

    druga wersja cwiczenia 3

--- cwiczenie3.py
+++ cwiczenie3.py
@@ -1,7 +1,8 @@
 #podwojna petla do tabliczki mnozenia
 
 for i in range (1,10):
-    for j in range (1,10):
-        print(f"{i} x {j} = {i*j}")
+    if i % 2 == 0:
+        for j in range (1,10):
+            print(f"{i} x {j} = {i*j}")
     print () #dla lepszej czytelnosci
 
