0a3bc22748883a7e39513785e4645b2bec9c5c3e
Bambo <student-wroclaw@EDU-WSB.local>
2024-12-07 13:51:21 +0100

    Zadanie pierwsze

---
+++ cwiczenie1.py
@@ -0,0 +1,15 @@
+# dane od uzytkownika
+number1 = float(input("Podaj pierwszą liczbę"))
+number2 = float(input("Podaj drugą liczbę"))
+
+# operacja matematyczna
+add = number1 + number2
+iloczyn = number1 * number2
+potęgowanie = number1 ** number2
+
+#wyswietlanie wyniku
+
+print(f"Suma Twoich liczb {number1} oraz {number2} to {add}")
+print(f"Iloczyn Twoich liczb {number1} oraz {number2} to {iloczyn}")
+print(f"Potęga Twoich liczb {number1} oraz {number2} to {potęgowanie}")
+
